f1f932f68a8a3d03c7774666bb601ce34d62bba5
Adrian L Lange <adrianlund@gmail.com>
2014-08-09 16:36:31 +0200

    Update with changes to all alerts and servers - Woodman and Miller merged, Ceres and Cobalt merged - Only territory alerts exist now

--- public.js
+++ public.js
@@ -2,19 +2,10 @@ var worlds, init;
 
 var worldNames = {
 	1: 'Connery',
-	9: 'Woodman',
 	10: 'Miller',
-	11: 'Ceres',
 	13: 'Cobalt',
 	17: 'Emerald',
 	25: 'Briggs'
-};
-
-var eventNames = {
-	0: 'Territory',
-	2: 'Amp Stations',
-	3: 'Biolabs',
-	4: 'Tech Plants'
 };
 
 var zoneNames = {
@@ -30,50 +21,6 @@ var factionAbbrevs = [
 	'tr'
 ];
 
-var facilityNames = {
-	// Indar
-	2105: 'Peris',
-	2107: 'Dahaka',
-	2109: 'Zurvan',
-	2103: 'Allatum',
-	2104: 'Saurva',
-	2106: 'Rashnu',
-	2101: 'Hvar',
-	2102: 'Mao',
-	2108: 'Tawrich',
-
-	// Hossin
-	4140: 'Naum',
-	4150: 'Hurakan',
-	4160: 'Ixtab',
-	4170: 'Acan',
-	4180: 'Bitol',
-	4190: 'Zotz',
-	4200: 'Ghanan',
-	4210: 'Mulac',
-	4220: 'Chac',
-
-	// Amerish
-	6101: 'Kwahtee',
-	6111: 'Sungrey',
-	6121: 'Wokuk',
-	6102: 'Ikanam',
-	6113: 'Onatha',
-	6123: 'Xelas',
-	6103: 'Heyoka',
-	6112: 'Mekala',
-	6122: 'Tumas',
-
-	// Esamir
-	18023: 'Elli',
-	18024: 'Freyr',
-	18027: 'Nott',
-	18022: 'Andvari',
-	18026: 'Mani',
-	18028: 'Ymir',
-	18025: 'Eisa'
-};
-
 var updateTime = function(){
 	var now = Date.now();
 
@@ -81,13 +28,13 @@ var updateTime = function(){
 		var alert = worlds[id].alert;
 		if(alert.active){
 			var date = new Date(alert.start - now);
-			date.setUTCHours(date.getUTCHours() + alert.duration);
+			date.setUTCHours(date.getUTCHours() + 1);
 
 			var h = date.getUTCHours();
 			var m = ('0' + date.getUTCMinutes()).slice(-2);
 			var s = ('0' + date.getUTCSeconds()).slice(-2);
 
-			if(h > 2 || (h + +m + +s) < 0){
+			if(h > 1 || (h + +m + +s) < 0){
 				alert.active = false;
 				updateAlert(id, alert);
 			} else
@@ -104,30 +51,14 @@ var updateDetails = function(id, details){
 	if(!alert.active)
 		return;
 
-	if(!alert.type){
-		var total = details[1].length + details[2].length + details[3].length;
-		for(var factionId in details){
-			var percentage = (details[factionId].length / total) * 100;
+	var total = details[1].length + details[2].length + details[3].length;
+	for(var factionId in details){
+		var percentage = (details[factionId].length / total) * 100;
 
-			$('<div></div>', {
-				class: factionAbbrevs[factionId - 1],
-				'data-title': Math.round(percentage) + '%'
-			}).css('width', (216 * percentage) / 100 + 'px').appendTo(field);
-		}
-	} else {
-		for(var factionId in details){
-			var regions = details[factionId];
-			for(var regionId = 0; regionId < regions.length; regionId++){
-				var region = regions[regionId];
-
-				$('<div></div>', {
-					class: factionAbbrevs[factionId - 1],
-					'data-title': facilityNames[region]
-				}).css({
-					'background-image': 'url(images/' + alert.type + '-' + factionId + '.png)'
-				}).appendTo(field);
-			}
-		}
+		$('<div></div>', {
+			class: factionAbbrevs[factionId - 1],
+			'data-title': Math.round(percentage) + '%'
+		}).css('width', (216 * percentage) / 100 + 'px').appendTo(field);
 	}
 };
 
@@ -142,16 +73,13 @@ var updateAlert = function(id, alert){
 	var schema = '#world-' + id;
 	if(alert.active){
 		$(schema).addClass('active');
-		$(schema).addClass(eventNames[alert.type].toLowerCase().replace(' ', ''));
 		$(schema + ' .state').html('Active alert!');
-		$(schema + ' .type').html(eventNames[alert.type]);
 		$(schema + ' .zone').html(zoneNames[alert.zone]);
 
 		updateTime();
 		updateDetails(id, worlds[id].details);
 	} else {
 		$(schema).removeClass();
-		$(schema + ' .type').html('');
 		$(schema + ' .zone').html('');
 		$(schema + ' .details').html('');
 
@@ -179,7 +107,6 @@ var processData = function(data){
 				$('table').append('<tr id="world-' + item.id + '"></tr>');
 				$('tr:last').append('<td>' + item.name + '</td>');
 				$('tr:last').append('<td class="state"></td>');
-				$('tr:last').append('<td class="type"></td>');
 				$('tr:last').append('<td class="zone"></td>');
 				$('tr:last').append('<td class="details"></td>');
 
